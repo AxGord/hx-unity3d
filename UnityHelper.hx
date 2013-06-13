28e98abdbb06c69e4e3ed7a7f0c5ea29c5ff20e3
AxG <axgord@gmail.com>
2013-06-13 18:12:01 +0600

    haxe3 support

--- UnityHelper.hx
+++ UnityHelper.hx
@@ -10,33 +10,33 @@ class UnityHelper {
 		return a;
 	}
 	
-	public static inline function getTransform(c:Component):unityEngine.Transform return untyped c.transform
-	public static inline function getRenderer(c:Component):Renderer return untyped c.renderer
-	public static inline function getRigidbody(c:Component):unityEngine.Rigidbody return untyped c.rigidbody
-	public static inline function getCamera(c:Component):unityEngine.Camera return untyped c.camera
-	public static inline function getLight(c:Component):unityEngine.Light return untyped c.light
-	public static inline function getAnimation(c:Component):unityEngine.Animation return untyped c.animation
-	public static inline function getConstantForce(c:Component):unityEngine.ConstantForce return untyped c.constantForce
-	public static inline function getGameObject(c:Component):unityEngine.GameObject return untyped c.gameObject
-	public static inline function getGuiTexture(c:Component):unityEngine.GUITexture return untyped c.guiTexture
+	public static inline function getTransform(c:Component):unityEngine.Transform return untyped c.transform;
+	public static inline function getRenderer(c:Component):Renderer return untyped c.renderer;
+	public static inline function getRigidbody(c:Component):unityEngine.Rigidbody return untyped c.rigidbody;
+	public static inline function getCamera(c:Component):unityEngine.Camera return untyped c.camera;
+	public static inline function getLight(c:Component):unityEngine.Light return untyped c.light;
+	public static inline function getAnimation(c:Component):unityEngine.Animation return untyped c.animation;
+	public static inline function getConstantForce(c:Component):unityEngine.ConstantForce return untyped c.constantForce;
+	public static inline function getGameObject(c:Component):unityEngine.GameObject return untyped c.gameObject;
+	public static inline function getGuiTexture(c:Component):unityEngine.GUITexture return untyped c.guiTexture;
 	
-	public static inline function chRenderer(c:Component):Renderer return c.GetComponentInChildren(Renderer)
+	public static inline function chRenderer(c:Component):Renderer return c.GetComponentInChildren(Renderer);
 	
-	@:extern public static inline function getComponentsInChildren<T>(c:Component, cl:Class<T>):Array<T> return toArray(c.GetComponentsInChildren(cl))
+	@:extern public static inline function getComponentsInChildren<T>(c:Component, cl:Class<T>):Array<T> return toArray(c.GetComponentsInChildren(cl));
 	
-	public static inline function chsRenderers(c:Component):Array<Renderer> return getComponentsInChildren(c, Renderer)
+	public static inline function chsRenderers(c:Component):Array<Renderer> return getComponentsInChildren(c, Renderer);
 }
 
 class UnityHelperGO {
-	public static inline function getTransform(c:GameObject):unityEngine.Transform return c.transform
-	public static inline function getRenderer(c:GameObject):unityEngine.Renderer return c.renderer
-	public static inline function getRigidbody(c:GameObject):unityEngine.Rigidbody return c.rigidbody
-	public static inline function getCamera(c:GameObject):unityEngine.Camera return c.camera
-	public static inline function getLight(c:GameObject):unityEngine.Light return c.light
-	public static inline function getAnimation(c:GameObject):unityEngine.Animation return c.animation
-	public static inline function getConstantForce(c:GameObject):unityEngine.ConstantForce return c.constantForce
-	public static inline function getGameObject(c:GameObject):unityEngine.GameObject return c
-	public static inline function getGuiTexture(c:GameObject):unityEngine.GUITexture return c.guiTexture
+	public static inline function getTransform(c:GameObject):unityEngine.Transform return c.transform;
+	public static inline function getRenderer(c:GameObject):unityEngine.Renderer return c.renderer;
+	public static inline function getRigidbody(c:GameObject):unityEngine.Rigidbody return c.rigidbody;
+	public static inline function getCamera(c:GameObject):unityEngine.Camera return c.camera;
+	public static inline function getLight(c:GameObject):unityEngine.Light return c.light;
+	public static inline function getAnimation(c:GameObject):unityEngine.Animation return c.animation;
+	public static inline function getConstantForce(c:GameObject):unityEngine.ConstantForce return c.constantForce;
+	public static inline function getGameObject(c:GameObject):unityEngine.GameObject return c;
+	public static inline function getGuiTexture(c:GameObject):unityEngine.GUITexture return c.guiTexture;
 	
 	
 }
